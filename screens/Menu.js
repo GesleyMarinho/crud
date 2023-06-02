5e5de256b8cc1e7da279c8294de7b973fe1cf855
GesleyMarinho <gesleymarinho@gmail.com>
2023-06-01 23:47:33 -0300

    Tela de Menu OKey , agora iniciar as operações

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,28 +1,59 @@
-import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import Constants from "expo-constants";
+import React from "react";
+import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-const Menu = () => {
+const Menu = ({ navigation }) => {
+  const listarCadastro = () => {
+    console.log("listar cadastro");
+  };
+
+  const deletarUser = () => {
+    console.log("Deletar User !");
+  };
+
+  const atualizarCadastro = () => {
+    console.log("Atualizar cadastro !");
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>Menu de opções aqui !</Text>
+      <TouchableOpacity style={styles.button} onPress={listarCadastro}>
+        <Text style={styles.buttonText}>Listar Cadastros</Text>
+        <Icon name="users" size={20} color="#FFFFFF" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={deletarUser}>
+        <Text style={styles.buttonText}>Deletar User</Text>
+        <Icon name="user-secret" size={20} color="#FFFFFF" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={atualizarCadastro}>
+        <Text style={styles.buttonText}>Atualizar Cadastros</Text>
+        <Icon name="user-plus" size={20} color="#FFFFFF" />
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: "#ecf0f1",
-    padding: 8,
+    marginTop: 20,
+    marginBottom: 20,
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
+  button: {
+    backgroundColor: "#FF4500",
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 5,
+    marginBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 20,
     fontWeight: "bold",
-    textAlign: "center",
+    marginRight: 10,
   },
 });
 
